42db2dc799d0cf54754dbaeae405055ecdb5a98b
dani-v1 <dani.vrind@gmail.com>
2025-10-02 18:28:50 +0200

    wereld "hart"
    als de wereld hart wordt gepakt gaat het naar de volgende scene

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -3,22 +3,47 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine.UI;
 
 
 public class Portal : MonoBehaviour
 {
     public int Lvl = 1;
+    public GameObject UI;
 
     public Rigidbody rb;
-     void OnTriggerEnter(Collider touch)
+    bool in_range = false;
+    private void Update()
     {
-        if (touch.CompareTag("Player"))
+        if (in_range)
         {
-        SceneManager.LoadScene(Lvl);
-        Lvl += 1;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("E pressed");
+                SceneManager.LoadScene(Lvl);
+                Lvl += 1;
+
+            }
         }
-       
     }
+        void OnTriggerEnter(Collider touch)
+        {
+            if (touch.CompareTag("Player") && gameObject.CompareTag("Portal"))
+            {
+                SceneManager.LoadScene(Lvl);
+                Lvl += 1;
+            }
+
+            if (touch.CompareTag("Player") && gameObject.CompareTag("Heart"))
+            {
+
+                UI.SetActive(true);
+                in_range = true;
+            }
+            else { UI.SetActive(false); }
+
+        }
 
 
+    
 }
