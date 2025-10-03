43daa39feebe3b56ba6ebfcc740d2b21354f46c5
ExVee <RosaAthenaMaes@gmail.com>
2025-10-03 13:50:07 +0200

    player movement works this time

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -9,25 +9,34 @@ public class Example : MonoBehaviour
 	[SerializeField] private float	jumpHeight = 1.5f;
 	[SerializeField] private float	gravityValue = -9.81f;
 	
-	[SerializeField]
-	private CharacterController controller;
-	private Vector3 playerVelocity;
-	private bool groundedPlayer;
+	[SerializeField] private CharacterController controller;
+	[SerializeField] private Collider collide;
 
+	[SerializeField] bool grounded;
+	[SerializeField] private Vector3 playerVelocity;
 	public InputActionReference moveAction; // expects Vector2
 	public InputActionReference jumpAction; // expects Button
 
+	private float	playerBoundExtent;
+
 	private void Start()
 	{
-
+		playerBoundExtent = collide.bounds.extents.y;
 	}
 
 	void Update()
 	{
-		groundedPlayer = controller.isGrounded;
-		if (groundedPlayer && playerVelocity.y < 0)
+		grounded = isGrounded();
+		
+		if (grounded)
 		{
-			playerVelocity.y = 0f;
+			playerVelocity.y = 0;
+		}
+		else if (!grounded)
+		{
+			Debug.Log("in the air");
+			//Apply gravity
+			playerVelocity.y += gravityValue * Time.deltaTime;
 		}
 
 		// Read input
@@ -35,22 +44,21 @@ public class Example : MonoBehaviour
 		Vector3 move = new Vector3(input.x, 0, input.y);
 		move = Vector3.ClampMagnitude(move, 1f);
 
-		if (move != Vector3.zero)
-		{
-			transform.forward = move;
-		}
-
 		// Jump
-		if (jumpAction.action.triggered && groundedPlayer)
+		if (jumpAction.action.triggered && grounded)
 		{
 			playerVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
 		}
 
-		// Apply gravity
-		playerVelocity.y += gravityValue * Time.deltaTime;
+
 
 		// Combine horizontal and vertical movement
 		Vector3 finalMove = (move * playerSpeed) + (playerVelocity.y * Vector3.up);
 		controller.Move(finalMove * Time.deltaTime);
+	}
+
+	bool isGrounded()
+	{
+		return (Physics.Raycast(transform.position, -Vector3.up, collide.bounds.extents.y + 0.2f));
 	}
 }
