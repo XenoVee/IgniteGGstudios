28975a17601da33e74db0e3d6fa8e1ab9c55c155
dani-v1 <dani.vrind@gmail.com>
2025-10-06 20:28:30 +0200

    hovering effect

---
+++ Assets/Scripts/hovering.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Hovering : MonoBehaviour
+{
+
+    private float x;
+    private float z;
+    private float startY;
+    public float speed = 1f;
+    public float amplitude = 0.5f;
+
+    private void Start()
+    {
+        x = transform.position.x;
+        z = transform.position.z;
+        startY = transform.position.y;
+    }
+    private void Update()
+    {
+        transform.position = new Vector3(x, startY + Mathf.Sin(Time.time*speed) * amplitude, z);
+    }
+}
